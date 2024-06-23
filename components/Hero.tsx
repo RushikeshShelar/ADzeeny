3dbd65e4a845b29400a208bd153e8c4bd103962e
Rushi <rushikeshshelar.cs@gmail.com>
2024-06-23 18:28:14 +0530

    Temp test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,28 @@ import Button from "@/components/ui/button";
 import { Spotlight } from "@/components/ui/spotlight";
 
 const Hero = () => {
+    const properties = [
+        {
+            src: "/quality.png",
+            alt: "Quality Assurance",
+            label: "Quality Assurance"
+        },
+        {
+            src: "/pricing.png",
+            alt: "Competetive Pricing",
+            label: "Competetive Pricing"
+        },
+        {
+            src: "/team.png",
+            alt: "Experienced Team",
+            label: "Experienced Team"
+        },
+        {
+            src: "/support.png",
+            alt: "Excellent Support",
+            label: "Excellent Support"
+        },
+    ];
     return (
         <>
             {/* Adding Spotlight Here */}
@@ -14,7 +36,7 @@ const Hero = () => {
             <Spotlight className="top-10 md:left-56 md:-top-10 h-[80vh] w-[50vw] hidden md:block" fill="blue" />
 
 
-            
+
 
 
 
@@ -25,44 +47,60 @@ const Hero = () => {
             </div>
 
 
-            <main className="w-full h-screen flex flex-col mt-20 justify-center md:justify-start items-center px-10 md:px-20            ">
-
-                <div className="h-3/4 flex flex-col justify-center md:items-end text-center md:text-start gap-6 md:flex-row md:gap-x-20">
-                    {/*TODO: LOGO HERE  */}
-                    {/* <Image
+            <main className="gap-0">
+                <div className="w-full h-screen flex flex-col mt-52 justify-center md:justify-start items-center px-10 md:px-20">
+                    <div className="h-3/4 flex flex-col justify-center items-center text-center md:text-start gap-6 md:flex-row md:gap-x-20">
+                        {/*TODO: LOGO HERE  */}
+                        {/* <Image
                     src=""
                     alt="ADZEENY"
-                /> */}
+                    /> */}
 
-
-
-                    <div className="flex flex-col justify-center gap-5 md:gap-10 md:max-w-[45vw] md:px-20">
-                        <TextGenerateEffect
-                            words="Wish for More Customers? Your Advertisiment Genie is right here."
-                            className="text-3xl md:text-4xl tracking-widest"
-                        />
-                        <div className="tracking:wider md:text-lg font-normal">
-                            {/* TODO: Figure out which is Better */}
-                            Our comprehensive digital solutions will completely transform your company. Grow the future of your business.
-                            {/* <TextGenerateEffect
+                        <div className="flex flex-col justify-center gap-5 md:gap-10 md:max-w-[45vw] md:px-20">
+                            <TextGenerateEffect
+                                words="Wish for More Customers? Your Advertisiment Genie is right here."
+                                className="text-3xl md:text-4xl tracking-widest"
+                            />
+                            <div className="tracking:wider md:text-lg font-normal">
+                                {/* TODO: Figure out which is Better */}
+                                Our comprehensive digital solutions will completely transform your company. Grow the future of your business.
+                                {/* <TextGenerateEffect
                                 words=" Our comprehensive digital solutions will completely transform your company. Grow the future of your business."
                                 className="font-medium"
-                            /> */}
+                                /> */}
+                            </div>
+                            <div className="flex flex-col gap-3 md:flex-row">
+                                <Button label="Get a Free Quote" />
+                                <Button label="Our Services" variant="default" />
+                            </div>
                         </div>
-                        <div className="flex flex-col gap-3 md:flex-row">
-                            <Button label="Get a Free Quote" />
-                            <Button label="Our Services" variant="default" />
+
+
+                        <div className="mt-10 md:mt-0 relative h-[30rem] w-[35rem]">
+                            <Image
+                                src={'/main.jpg'}
+                                alt="Landing page image"
+                                fill
+                            />
                         </div>
+
                     </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-3 w-full  dark:bg-black-100">
+                        {properties.map((item, index) => (
+                            <div className={`h-44 flex flex-col items-center justify-center tracking-wide text-white gap-6 ${index % 2 === 0 ? 'bg-[#22143D]' : 'bg-[#2F2248]'} `} key={index}>
+                                <div className="relative h-16 w-16">
+                                    <Image
+                                        src={`${item.src}`}
+                                        alt="QA"
+                                        fill
+                                    />
+                                </div>
+                                <div>
+                                    {item.label}
+                                </div>
+                            </div>
+                        ))}
 
-
-                    <div className="mt-10 md:mt-0">
-                        <Image
-                            src={'/main.jpg'}
-                            alt="Landing page image"
-                            height={800}
-                            width={800}
-                        />
                     </div>
                 </div>
 
